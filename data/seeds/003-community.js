f809b51d845e34446ddc68198e0ce9fa92a3c2ec
Manju KC <Manju KC>
2019-06-27 16:40:23 -0400

    updated codes

--- data/seeds/003-community.js
+++ data/seeds/003-community.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex, Promise) {
+ 
   return knex('community').del()
     .then(function () {
-     
       return knex('community').insert([
-        {community:"Yokohama", country_id:1},
-        {community:"Rosario", country_id:2},
-        {community:"Andulo", country_id:3}
+        {community:"yokohama", country_id:1},
+        {community:"sangai", country:2},
+        {community:"saringo", country:3}
       ]);
     });
 };
